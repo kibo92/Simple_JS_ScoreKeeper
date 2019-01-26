641df05b1b6fe90885381cb335c57e476ceb2794
Bach <borislukovic@outlook.com>
2019-01-26 20:57:30 +0100

    FirstCommit
    Frontpage and js logic for first two buttons and score's

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// console.log("Connected JS");
+var p1Button = document.getElementById("p1");
+var p1Score = 0;
+var p1Display = document.getElementById("p1Display");
+var p2Button = document.getElementById("p2");
+var p2Score = 0;
+var p2Display = document.getElementById("p2Display");
+var reset = document.getElementById("reset");
 
+p1Button.addEventListener("click", function() {
+    p1Score++;
+    p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click",  function() {
+    p2Score++;
+    p2Display.textContent = p2Score;
+});
