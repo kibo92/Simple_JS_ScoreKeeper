4c5142c0b6facf3f717345d7466696b44218abf2
Bach <borislukovic@outlook.com>
2019-01-27 01:45:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,28 @@ var p2Score = 0;
 var p2Display = document.getElementById("p2Display");
 var reset = document.getElementById("reset");
 
+var gameOver = false;
+var winningScore = 5;
+
 p1Button.addEventListener("click", function() {
-    p1Score++;
-    p1Display.textContent = p1Score;
+    if(!gameOver) {
+        p1Score++;
+        if(p1Score === winningScore) {
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+    
+    
 });
 
 p2Button.addEventListener("click",  function() {
-    p2Score++;
-    p2Display.textContent = p2Score;
+    if(!gameOver) {
+        p2Score++;
+        if(p2Score === winningScore) {
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+    
 });
