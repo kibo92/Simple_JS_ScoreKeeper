488109c5d344b68141525fa9372c1dec826e19a6
Bach <borislukovic@outlook.com>
2019-01-28 01:24:53 +0100

    Reset but functions and winner style
    Adding css style for winner h1 span color and adding reset button js logic

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var p1Display = document.getElementById("p1Display");
 var p2Button = document.getElementById("p2");
 var p2Score = 0;
 var p2Display = document.getElementById("p2Display");
-var reset = document.getElementById("reset");
+var resetButton = document.getElementById("reset");
 
 var gameOver = false;
 var winningScore = 5;
@@ -13,6 +13,7 @@ p1Button.addEventListener("click", function() {
     if(!gameOver) {
         p1Score++;
         if(p1Score === winningScore) {
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -25,9 +26,20 @@ p2Button.addEventListener("click",  function() {
     if(!gameOver) {
         p2Score++;
         if(p2Score === winningScore) {
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
     
+});
+
+resetButton.addEventListener("click", function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 });
